d306d56bdf02a521b33d7c24d359bd7c8bce2cb6
Ashish Pisey <ashishpisey21@gmail.com>
2021-12-05 15:31:16 +0530

    Scroll view stack views

---
+++ ScrollView Demo/ScrollView Demo/SecondViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SecondViewController.swift
+//  ScrollView Demo
+//
+//  Created by Ashish Pisey on 05/12/21.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
---
+++ ScrollView Demo/ScrollView Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ScrollView Demo
+//
+//  Created by Ashish Pisey on 05/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    // MARK: - Navigation
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+    }
+
+
+}
+
