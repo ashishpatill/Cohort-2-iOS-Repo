a9cba84406ae552b526df34c59e489d2bc4616e5
Ashish Pisey <ashishpisey21@gmail.com>
2021-12-12 16:58:41 +0530

    TableViewController Demo

---
+++ TableVCDemo/TableVCDemo/AddItemVC.swift
@@ -0,0 +1,44 @@
+//
+//  AddItemVC.swift
+//  TableVCDemo
+//
+//  Created by Ashish Pisey on 12/12/21.
+//
+
+import UIKit
+
+struct Task {
+    let id : String
+    let name : String
+    let status :  Bool
+}
+
+protocol AddItemDelegate: NSObjectProtocol {
+    func sendTask(taskObj:Task)
+}
+
+class AddItemVC: UIViewController {
+    var taskName: String?
+    @IBOutlet weak var taskTextField: UITextField!
+    weak var addItemDelegate: AddItemDelegate? // task list
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+        //print("task received: \(taskName!)")
+    }
+    
+
+    @IBAction func addItemAction(_ sender: UIButton) {
+        
+        // pass task to task list screen
+        if let task = self.taskTextField.text {
+            let taskObj = Task.init(id:  "123", name: task, status: false)
+            print("Add item button pressed")
+            addItemDelegate?.sendTask(taskObj: taskObj)
+            self.navigationController?.popViewController(animated: true) // go back
+        }
+    }
+
+}
---
+++ TableVCDemo/TableVCDemo/TaskListVC.swift
@@ -0,0 +1,102 @@
+//
+//  ItemList.swift
+//  TableVCDemo
+//
+//  Created by Ashish Pisey on 12/12/21.
+//
+
+import UIKit
+
+class TaskListVC: UITableViewController, AddItemDelegate {
+    var itemList = ["Buy groceries", "Get milk", "Order cookies"]
+    var headerTitleArr = ["Groceries", "Vegetables", "Cereals"]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    @IBAction func addAction(_ sender: UIBarButtonItem) {
+        let addItemVC = storyboard?.instantiateViewController(withIdentifier: "ItemVCID") as! AddItemVC
+        addItemVC.taskName = "Hello"
+        addItemVC.addItemDelegate = self
+        self.navigationController?.pushViewController(addItemVC, animated: true) // go to next screen
+    }
+    
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return headerTitleArr[section]
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return itemList.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "taskCell", for: indexPath)
+        cell.textLabel?.text = itemList[indexPath.row]
+        return cell
+    }
+    
+    func sendTask(taskObj: Task) {
+        print("received task in Task List VC")
+        itemList.append(taskObj.name)
+        tableView.reloadData()
+    }
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
